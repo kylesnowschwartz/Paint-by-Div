81059ce54dbca9b82488a54523ca2fea30e5cb16
samnz19 <samnz19@gmail.com>
2015-06-04 15:37:16 +1200

    not working rainbow

--- application.js
+++ application.js
@@ -26,8 +26,6 @@ Easel.prototype.renderGrid = function () {
   for (var r = 0; r < (this.height / 9); r++) {
     var $row = $("<div class='row'></div>");
     for (var c = 0; c < (this.width / 8); c++) {
-      console.log (this.grid[r][c])
-      // console.log(this.grid)
       var $cell = this.grid[r][c].$element;
       $row.append($cell);
     }
@@ -36,8 +34,33 @@ Easel.prototype.renderGrid = function () {
 };
 
   $(document).on('mouseover', '.blank', function (e) {
-    console.log("log")
     var $link = $(this);
     $link.addClass('red');
   });
 
+
+
+  function rainbow(noOfColors){
+    frequency=5/noOfColors;
+    for (var i = 0; i < noOfColors; ++i){
+      r =   Math.sin(frequency*i + 0) * (127) + 128;
+      g =   Math.sin(frequency*i + 2) * (127) + 128;
+      b =   Math.sin(frequency*i + 4) * (127) + 128;
+      div = $("<div class='rainbow'></div>");
+      div.replace("{r}",Math.floor(r));
+      div.replace("{g}",Math.floor(g));
+      div.replace("{b}",Math.floor(b));
+    }
+  }
+
+
+  $(document).on('keydown',function(e){
+    if (e.keyCode == 82){
+      var $link = $(this);
+      $link.removeClass('red');
+      $link.addClass('rainbow')
+      $(document).on('mouseover','.ranbow',function(){
+        $( this ).fadeTo( "slow", 0.20 );
+      });
+    } 
+  });
