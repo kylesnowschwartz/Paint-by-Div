2a2cd485876c04205b29099787da760c8fde88df
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-06-05 10:23:25 +1200

    adding changes

--- application.js
+++ application.js
@@ -2,58 +2,28 @@ function Cell () {
   this.$element = $("<div class='blank'></div>");
 }
 
-function Easel (width, height) {
-  this.width = width;
-  this.height = height;
-  this.grid = this.generateGrid();
-}
 
-Easel.prototype.generateGrid = function () {
+// var appendClass = ''
+// var changeClass = ''
+// $(document).on("keydown", function(e) {
 
-  var grid = [];
-  for (var r = 0; r < Math.floor(this.height / 8) ; r++) {
-    var row = [];
-    for (var c = 0; c < Math.floor(this.width / 8); c++) {
-      row.push(new Cell());
-    }
-    grid.push(row);
-  }
-  return grid;
-};
+//   if(e.keyCode == 65) {
+//     changeClass = 'red'
+//   }
+//   else if(e.keyCode == 83) {
+//     changeClass = 'blue'
+//   }
+//   else if(e.keyCode == 68) {
+//     appendClass = 'circle'
+//   }
+//   else if(e.keyCode == 87) {
+//     changeClass = 'joshua'
+//   }
+//   else if(e.keyCode == 82) {
+//     changeClass = 'rainbow'
+//   };
 
-Easel.prototype.renderGrid = function () {
-  for (var r = 0; r < Math.floor(this.height / 8); r++) {
-    var $row = $("<div class='row'></div>");
-    for (var c = 0; c < Math.floor(this.width / 8); c++) {
-      var $cell = this.grid[r][c].$element;
-      $row.append($cell);
-    }
-    $('#grid').append($row);
-  }
-};
-
-
-var appendClass = ''
-var changeClass = ''
-$(document).on("keydown", function(e) {
-
-  if(e.keyCode == 65) {
-    changeClass = 'red'
-  }
-  else if(e.keyCode == 83) {
-    changeClass = 'blue'
-  }
-  else if(e.keyCode == 68) {
-    appendClass = 'circle'
-  }
-  else if(e.keyCode == 87) {
-    changeClass = 'joshua'
-  }
-  else if(e.keyCode == 82) {
-    changeClass = 'rainbow'
-  };
-
-});
+// });
 
   $(document).on('mousemove', '.blank', function (e) {
     var $div = $(this);
@@ -91,3 +61,32 @@ $(document).on("keydown", function(e) {
     }, 10000);
 
 
+function Easel (width, height) {
+  this.width = width;
+  this.height = height;
+  this.grid = this.generateGrid();
+}
+
+// Easel.prototype.generateGrid = function () {
+
+//   var grid = [];
+//   for (var r = 0; r < Math.floor(this.height / 8) ; r++) {
+//     var row = [];
+//     for (var c = 0; c < Math.floor(this.width / 8); c++) {
+//       row.push(new Cell());
+//     }
+//     grid.push(row);
+//   }
+//   return grid;
+// };
+
+// Easel.prototype.renderGrid = function () {
+//   for (var r = 0; r < Math.floor(this.height / 8); r++) {
+//     var $row = $("<div class='row'></div>");
+//     for (var c = 0; c < Math.floor(this.width / 8); c++) {
+//       var $cell = this.grid[r][c].$element;
+//       $row.append($cell);
+//     }
+//     $('#grid').append($row);
+//   }
+// };
