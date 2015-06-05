47193e69a8b1199ae9481145755c44c5af5cc6b3
Kyle Snow Schwartz <kyle.snowschwartz@gmail.com>
2015-06-05 12:07:20 +1200

    create divs on mouseover functioning

--- application.js
+++ application.js
@@ -1,71 +1,88 @@
+$(document).ready(function() {
+
 function Cell () {
-  this.$element = $("<div class='blank'></div>");
+  this.div = $("<div class='blank'></div>");
 }
 
+$(document).mousemove(function(e){
+  var mouseX = e.pageX
+  var mouseY = e.pageY
 
-// var appendClass = ''
-// var changeClass = ''
-// $(document).on("keydown", function(e) {
+  $div = $("<div class='blank'></div>");
+  $div.addClass(changeClass);
+  $div.addClass(appendClass);
 
-//   if(e.keyCode == 65) {
-//     changeClass = 'red'
-//   }
-//   else if(e.keyCode == 83) {
-//     changeClass = 'blue'
-//   }
-//   else if(e.keyCode == 68) {
-//     appendClass = 'circle'
-//   }
-//   else if(e.keyCode == 87) {
-//     changeClass = 'joshua'
-//   }
-//   else if(e.keyCode == 82) {
-//     changeClass = 'rainbow'
-//   };
+  $($div).css({'top':mouseY,
+            'left':mouseX,
+            'position':'fixed'});
 
-// });
+  $('#canvas-div').append($div);
 
-  $(document).on('mousemove', '.blank', function (e) {
-    var $div = $(this);
-    $div.removeClass();
-    $div.addClass('blank')
-    $div.addClass(changeClass);
-    $div.addClass(appendClass);
-    // $('rainbow').html(getColor(251));
-  });
+});
 
-    function getColor(noOfColors){
-        htmlString ="";
-        frequency=5/noOfColors;
-        for (var i = 0; i < noOfColors; ++i){
-            r =   Math.sin(frequency*i + 0) * (127) + 128;
-            g =   Math.sin(frequency*i + 2) * (127) + 128;
-            b =   Math.sin(frequency*i + 4) * (127) + 128;
-            div='<div class="color" style="background-color:rgb({r},{g},{b});">  </div>';
-            div=div.replace("{r}",Math.floor(r));
-            div=div.replace("{g}",Math.floor(g));
-            div=div.replace("{b}",Math.floor(b));
-            htmlString  += div;
-        }
-        return htmlString ;
-    }
-    $('#rainbow').on('mouseover','.color',function(){
-        $( this ).fadeTo( "slow", 0.20 );
-     });
+var appendClass = ''
+var changeClass = ''
+$(document).on("keydown", function(e) {
 
-    $('#rainbow').html(getColor(251));
+  if(e.keyCode == 65) {
+    changeClass = 'red'
+  }
+  else if(e.keyCode == 83) {
+    changeClass = 'blue'
+  }
+  else if(e.keyCode == 68) {
+    appendClass = 'circle'
+  }
+  else if(e.keyCode == 87) {
+    changeClass = 'joshua'
+  }
+  else if(e.keyCode == 82) {
+    changeClass = 'rainbow'
+  };
+
+});
+
+//   $(document).on('mousemove', '.blank', function (e) {
+//     var $div = $(this);
+//     $div.removeClass();
+//     $div.addClass('blank')
+//     $div.addClass(changeClass);
+//     $div.addClass(appendClass);
+//     // $('rainbow').html(getColor(251));
+//   });
+
+//     function getColor(noOfColors){
+//         htmlString ="";
+//         frequency=5/noOfColors;
+//         for (var i = 0; i < noOfColors; ++i){
+//             r =   Math.sin(frequency*i + 0) * (127) + 128;
+//             g =   Math.sin(frequency*i + 2) * (127) + 128;
+//             b =   Math.sin(frequency*i + 4) * (127) + 128;
+//             div='<div class="color" style="background-color:rgb({r},{g},{b});">  </div>';
+//             div=div.replace("{r}",Math.floor(r));
+//             div=div.replace("{g}",Math.floor(g));
+//             div=div.replace("{b}",Math.floor(b));
+//             htmlString  += div;
+//         }
+//         return htmlString ;
+//     }
+//     $('#rainbow').on('mouseover','.color',function(){
+//         $( this ).fadeTo( "slow", 0.20 );
+//      });
+
+//     $('#rainbow').html(getColor(251));
 
 
-    setInterval(function() {
-      $('.color').fadeTo("slow", 1);
-    }, 10000);
+//     setInterval(function() {
+//       $('.color').fadeTo("slow", 1);
+//     }, 10000);
 
 
-function Easel (width, height) {
-  this.width = width;
-  this.height = height;
-  this.grid = this.generateGrid();
-}
+// function Easel (width, height) {
+//   this.width = width;
+//   this.height = height;
+//   this.grid = this.generateGrid();
+// }
 
 // Easel.prototype.generateGrid = function () {
 
@@ -90,3 +107,5 @@ function Easel (width, height) {
 //     $('#grid').append($row);
 //   }
 // };
+
+});
